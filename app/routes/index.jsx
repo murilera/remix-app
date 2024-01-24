a3ae77b104df4de23b06e63bc1f4f2c35fa1fc09
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2024-01-24 17:05:22 -0300

    feat: add ssa handler from remix

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,10 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Link to={"/demo"}>to demo page</Link>
+    </>
+  );
 }
